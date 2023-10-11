8a764a96b0d52c6b203a7621ee3886b8dfc424a2
danieltmarch <43455439+danieltmarch@users.noreply.github.com>
2023-10-10 19:25:58 -0700

    Modified load and auto-gen button behavior
    Load will autogen if no data exists
    autogen will not automatically save.

---
+++ Code/.ipynb_checkpoints/FitDef-checkpoint.py
@@ -0,0 +1,31 @@
+#fit data handles saving a characters fits and relevant info
+#this is the main format the app uses to draw and adjust
+import numpy as np
+import matplotlib.pyplot as plt
+from StrokeFit import Fit
+import StrokeFit as sf
+import pickle #for saving
+import os
+
+class FitData:
+    #character, the chinese character for this particular fit
+    #fits, a list of Fit types with bounding boxes for the arial unicode image
+    #size: [width, height] for the arial unicode image
+    def set(self, character, fits, size): #same thing as init
+        self.character = character
+        self.fits = fits
+        self.size = np.array(size)
+        
+#function to save/load a particular fit
+def loadFits(character):
+    try:
+        with open(f"../Data/Fits/{character}.pickle", 'rb') as file:
+            return pickle.load(file) #FitData
+    except IOError:
+        return None
+def saveFits(fitData):
+    with open(f"../Data/Fits/{fitData.character}.pickle", 'wb') as file:
+        pickle.dump(fitData, file)
+        
+def fileExists(character):
+    return os.path.exists(f"../Data/Fits/{character}.pickle")
---
+++ Code/.ipynb_checkpoints/FormalImage-checkpoint.py
@@ -0,0 +1,49 @@
+#handles rendering the formal image of a certain character, relatively simple.
+#alternatively we can use tkinter to make images, but the only difference would be image size and position
+#which shouldn't really matter
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 #can be installed with pip install opencv-python
+
+#Heavily based on https://stackoverflow.com/a/27753869/190597 (jsheperd)
+#renders as 1 = black, 0 = white
+#size is the final width/height of the image
+def renderChar(char, path='../Fonts/msyh.ttc', size=1000, fontsize=1000, pad=0.1, show=False, imgSize=12):
+    font = ImageFont.truetype(path, fontsize) 
+    w, h = font.getsize(char)
+    h *= 2
+    image = Image.new('L', (w, h), 1)  
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), char, font=font)
+    arr = np.asarray(image)
+    arr = np.where(arr, 0, 1)
+    arr = arr[(arr != 0).any(axis=1)]
+    
+    #to save some pain we'll flip the y axis so it reads like a normal graph
+    #arr = np.flip(arr, axis=0) #not currently used
+    
+    #we need some padding on the edges, 20% to height and width each
+    height = len(arr)
+    width = len(arr[0])
+    hPad = int(height*pad/2) #half pad on each side
+    wPad = int(width*pad/2)
+     
+    paddedImg = np.zeros( (height+2*hPad, width+2*wPad) )
+    paddedImg[ hPad:hPad+height , wPad:wPad+width ] = arr
+    
+    currDim = np.array(np.shape(paddedImg))
+    maxDim = max(currDim)
+    imgSquare = np.zeros((maxDim,maxDim))
+    offset = (maxDim - currDim)//2 #so image is centered
+    imgSquare[offset[0]:offset[0]+currDim[0], offset[1]:offset[1]+currDim[1]] = paddedImg
+
+    img = cv2.resize(imgSquare, dsize=(size, size), interpolation=cv2.INTER_NEAREST) #nearest neighbor interpolation
+    
+    if(show):
+        fig, ax = plt.subplots(figsize=(imgSize,imgSize))
+        ax.imshow(img, cmap='gray', vmin=0, vmax=20)
+    return img
